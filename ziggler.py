0afb281a72bec80907a59fa9e2555453d8e9cbfc
ecjoee <43061889+Ecjoe@users.noreply.github.com>
2023-09-12 21:06:23 +0530

    Add files via upload

---
+++ ziggler.py
@@ -0,0 +1,24 @@
+import pyautogui as pag
+import time
+
+pag.FAILSAFE=False
+
+mins = 2
+
+total_time = time.time() + 60 * mins
+
+while True:
+
+    search_bar_pos= (111,1050)
+    pag.moveTo(search_bar_pos)
+    pag.click()
+
+    time.sleep(15)
+    pag.typewrite('Notepad', 0.25)
+    pag.press('enter')
+    
+    while time.time() <= total_time:
+        time.sleep(15)
+        pag.typewrite("Boring Work! Don't do.\n", 0.25)
+        time.sleep(15)
+    break
